cc581b8fff923eeea67849917980f66424b27489
CryptoThibault <thibault.chalaoux@gmail.com>
2025-05-07 18:09:05 +0200

    pong

--- dist/game.js
+++ dist/game.js
@@ -1,28 +1,89 @@
 import { Ball } from "./ball.js";
 import { Paddle } from "./paddle.js";
+const MIN_SPEED = 3;
+const MAX_SPEED = 10;
+const SPEED_INC = 1.05;
+const PADDLE_SPEED = 5;
+const MAX_SCORE = 10;
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 let isRunning = true;
+let keys = {
+    "w": false,
+    "s": false,
+    "Up": false,
+    "Down": false
+};
 let scores = { left: 0, right: 0 };
 const dxStart = Math.floor(Math.random() * 2) ? -1 : 1;
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 5, dxStart, 0, 5, "white");
+const ball = new Ball(canvas.width / 2, canvas.height / 2, 5, dxStart, 0, MIN_SPEED, "white");
 const paddleWidth = canvas.width * 0.01;
 const paddleHeight = canvas.height * 0.15;
-const leftPaddle = new Paddle(5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 10, "white");
-const rightPaddle = new Paddle(canvas.width - paddleWidth - 5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 10, "white");
+const leftPaddle = new Paddle(5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, PADDLE_SPEED, "white");
+const rightPaddle = new Paddle(canvas.width - paddleWidth - 5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, PADDLE_SPEED, "white");
 function scoreGoal(player) {
     player ? scores.right++ : scores.left++;
-    ball.dx = player ? -1 : 1;
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
+    ball.dx = player ? 1 : -1;
+    ball.dy = 0;
+    ball.speed = MIN_SPEED;
+    if (scores.left === 10 || scores.right === 10)
+        quitGame();
 }
 export default scoreGoal;
+function hitPaddle(ball, paddle) {
+    return (ball.x - ball.radius < paddle.x + paddle.width &&
+        ball.x + ball.radius > paddle.x &&
+        ball.y - ball.radius < paddle.y + paddle.height &&
+        ball.y + ball.radius > paddle.y);
+}
+function onPaddleHit(ball, paddle) {
+    const paddleCenterY = paddle.y + paddle.height / 2;
+    ball.dy = (ball.y - paddleCenterY) / (paddle.height / 2);
+    ball.dx = -ball.dx;
+    const length = Math.hypot(ball.dx, ball.dy);
+    ball.dx /= length;
+    ball.dy /= length;
+    ball.speed = Math.min(ball.speed * SPEED_INC, MAX_SPEED);
+    ball.x += ball.dx * ball.radius;
+}
 function updateGame() {
     ball.move(canvas.width, canvas.height);
-    //ball.logPosition();
-    //leftPaddle.logPosition();
-    //rightPaddle.logPosition();
-    console.log(`Scores: left : ${scores.left} | right : ${scores.right}`);
+    if (keys.w)
+        leftPaddle.moveUp(canvas.height);
+    if (keys.s)
+        leftPaddle.moveDown(canvas.height);
+    if (keys.Up)
+        rightPaddle.moveUp(canvas.height);
+    if (keys.Down)
+        rightPaddle.moveDown(canvas.height);
+    if (hitPaddle(ball, leftPaddle))
+        onPaddleHit(ball, leftPaddle);
+    if (hitPaddle(ball, rightPaddle))
+        onPaddleHit(ball, rightPaddle);
+}
+function drawScores() {
+    ctx.font = "100px Tohama";
+    ctx.fillStyle = "white";
+    ctx.fillText(`${scores.left}`, canvas.width / 2 - 100, 120);
+    ctx.fillText(`${scores.right}`, canvas.width / 2 + 100, 120);
+}
+function drawCenterLine() {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 4;
+    ctx.stroke();
+    ctx.closePath();
+}
+function drawText() {
+    ctx.font = "40px Tahoma";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+    ctx.fillText("Pong".split('').join(' '.repeat(2)), canvas.width / 2, canvas.height - 10);
 }
 function renderGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -31,6 +92,9 @@ function renderGame() {
     ball.draw(ctx);
     leftPaddle.draw(ctx);
     rightPaddle.draw(ctx);
+    drawScores();
+    drawCenterLine();
+    drawText();
 }
 function renderPauseMenu() {
     ctx.fillStyle = "rgba(0, 0, 0, 0.7)";
@@ -38,22 +102,13 @@ function renderPauseMenu() {
     ctx.fillStyle = "white";
     ctx.font = "30px Arial";
     ctx.textAlign = "center";
-    ctx.fillText("PAUSE", canvas.width / 2, canvas.height / 2);
-    ctx.fillText("Press P to Resume", canvas.width / 2, canvas.height / 1.5);
-}
-function gameLoop() {
-    if (!isRunning)
-        return;
-    updateGame();
-    renderGame();
-    requestAnimationFrame(gameLoop);
+    ctx.fillText("PAUSE", canvas.width / 2, canvas.height / 3);
+    ctx.fillText("Press P to Resume", canvas.width / 2, canvas.height / 2);
+    ctx.fillText("Press ESC to Quit", canvas.width / 2, canvas.height / 1.5);
 }
 function togglePause() {
     isRunning = !isRunning;
-    if (isRunning)
-        requestAnimationFrame(gameLoop);
-    else
-        renderPauseMenu();
+    isRunning ? requestAnimationFrame(gameLoop) : renderPauseMenu();
 }
 function quitGame() {
     window.location.reload();
@@ -64,14 +119,33 @@ window.addEventListener("keydown", (event) => {
     if (event.key === "Escape")
         quitGame();
     if (isRunning) {
+        if (event.key === "w")
+            keys.w = true;
         if (event.key === "s")
-            leftPaddle.moveUp(canvas.height);
-        if (event.key === "x")
-            leftPaddle.moveDown(canvas.height);
+            keys.s = true;
         if (event.key === "ArrowUp")
-            rightPaddle.moveUp(canvas.height);
+            keys.Up = true;
         if (event.key === "ArrowDown")
-            rightPaddle.moveDown(canvas.height);
+            keys.Down = true;
     }
 });
+window.addEventListener('keyup', (event) => {
+    if (isRunning) {
+        if (event.key === "w")
+            keys.w = false;
+        if (event.key === "s")
+            keys.s = false;
+        if (event.key === "ArrowUp")
+            keys.Up = false;
+        if (event.key === "ArrowDown")
+            keys.Down = false;
+    }
+});
+function gameLoop() {
+    if (!isRunning)
+        return;
+    updateGame();
+    renderGame();
+    requestAnimationFrame(gameLoop);
+}
 gameLoop();
--- src/game.ts
+++ src/game.ts
@@ -1,36 +1,102 @@
 import { Ball } from "./ball.js";
 import { Paddle } from "./paddle.js"
 
+const MIN_SPEED = 3;
+const MAX_SPEED = 10;
+const SPEED_INC = 1.05;
+const PADDLE_SPEED = 5;
+const MAX_SCORE = 10;
+
 const canvas = document.getElementById("gameCanvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 let isRunning: boolean = true;
+let keys: { [key: string]: boolean } = {
+    "w": false,
+    "s": false,
+    "Up": false,
+    "Down": false
+};
 
 let scores = {left: 0, right: 0};
 
 const dxStart = Math.floor(Math.random() * 2) ? -1 : 1;
-const ball = new Ball(canvas.width / 2, canvas.height / 2, 5, dxStart, 0, 5, "white");
+const ball = new Ball(canvas.width / 2, canvas.height / 2, 5, dxStart, 0, MIN_SPEED, "white");
 
 const paddleWidth = canvas.width * 0.01;
-const paddleHeight = canvas.height * 0.15
-const leftPaddle = new Paddle(5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 10, "white");
-const rightPaddle = new Paddle(canvas.width - paddleWidth - 5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, 10, "white");
+const paddleHeight = canvas.height * 0.15;
+const leftPaddle = new Paddle(5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, PADDLE_SPEED, "white");
+const rightPaddle = new Paddle(canvas.width - paddleWidth - 5, canvas.height / 2 - paddleHeight / 2, paddleWidth, paddleHeight, PADDLE_SPEED, "white");
 
 function scoreGoal(player: number) {
     player ? scores.right++ : scores.left++;
-    ball.dx = player ? -1 : 1;
     ball.x = canvas.width / 2;
     ball.y = canvas.height / 2;
+    ball.dx = player ? 1 : -1;
+    ball.dy = 0;
+    ball.speed = MIN_SPEED;
+    if (scores.left === 10 || scores.right === 10)
+        quitGame();
 }
 
 export default scoreGoal;
 
+function hitPaddle(ball:Ball, paddle: Paddle): boolean {
+    return (
+        ball.x - ball.radius < paddle.x + paddle.width &&
+        ball.x + ball.radius > paddle.x &&
+        ball.y - ball.radius < paddle.y + paddle.height &&
+        ball.y + ball.radius > paddle.y
+    );
+}
+
+function onPaddleHit(ball: Ball, paddle: Paddle): void {
+    const paddleCenterY = paddle.y + paddle.height / 2;
+    ball.dy = (ball.y - paddleCenterY) / (paddle.height / 2);
+    ball.dx = -ball.dx;
+
+    const length = Math.hypot(ball.dx, ball.dy);
+    ball.dx /= length;
+    ball.dy /= length;
+    ball.speed = Math.min(ball.speed * SPEED_INC, MAX_SPEED);
+    ball.x += ball.dx * ball.radius;
+}  
+
 function updateGame() {
     ball.move(canvas.width, canvas.height);
-    //ball.logPosition();
-    //leftPaddle.logPosition();
-    //rightPaddle.logPosition();
-    console.log(`Scores: left : ${scores.left} | right : ${scores.right}`);
+    if (keys.w) leftPaddle.moveUp(canvas.height);
+    if (keys.s) leftPaddle.moveDown(canvas.height);
+    if (keys.Up) rightPaddle.moveUp(canvas.height);
+    if (keys.Down) rightPaddle.moveDown(canvas.height);
+    if (hitPaddle(ball, leftPaddle))
+        onPaddleHit(ball, leftPaddle);
+    if (hitPaddle(ball, rightPaddle))
+        onPaddleHit(ball, rightPaddle);
+}
+
+function drawScores(): void {
+    ctx.font = "100px Tohama";
+    ctx.fillStyle = "white";
+    ctx.fillText(`${scores.left}`, canvas.width / 2 - 100, 120);
+    ctx.fillText(`${scores.right}`, canvas.width / 2 + 100, 120);
+}
+
+function drawCenterLine(): void {
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2, 0);
+    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 4;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function drawText(): void {
+    ctx.font = "40px Tahoma";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "bottom";
+    ctx.fillText("Pong".split('').join(' '.repeat(2)), canvas.width / 2, canvas.height - 10);
 }
 
 function renderGame() {
@@ -40,6 +106,9 @@ function renderGame() {
     ball.draw(ctx);
     leftPaddle.draw(ctx);
     rightPaddle.draw(ctx);
+    drawScores();
+    drawCenterLine();
+    drawText();
 }
 
 function renderPauseMenu() {
@@ -49,24 +118,14 @@ function renderPauseMenu() {
     ctx.fillStyle = "white";
     ctx.font = "30px Arial";
     ctx.textAlign = "center";
-    ctx.fillText("PAUSE", canvas.width / 2, canvas.height / 2);
-    ctx.fillText("Press P to Resume", canvas.width / 2, canvas.height / 1.5);
-}
-
-function gameLoop() {
-    if (!isRunning) return;
-    updateGame();
-    renderGame();
-    requestAnimationFrame(gameLoop);
+    ctx.fillText("PAUSE", canvas.width / 2, canvas.height / 3);
+    ctx.fillText("Press P to Resume", canvas.width / 2, canvas.height / 2);
+    ctx.fillText("Press ESC to Quit", canvas.width / 2, canvas.height / 1.5);
 }
 
 function togglePause() {
     isRunning = !isRunning;
-
-    if (isRunning)
-        requestAnimationFrame(gameLoop);
-    else
-        renderPauseMenu();
+    isRunning ? requestAnimationFrame(gameLoop) : renderPauseMenu();
 }
 
 function quitGame() {
@@ -77,11 +136,27 @@ window.addEventListener("keydown", (event) => {
     if (event.key === "p") togglePause();
     if (event.key === "Escape") quitGame();
     if (isRunning) {
-        if (event.key === "s") leftPaddle.moveUp(canvas.height);
-        if (event.key === "x") leftPaddle.moveDown(canvas.height);
-        if (event.key === "ArrowUp") rightPaddle.moveUp(canvas.height);
-        if (event.key === "ArrowDown") rightPaddle.moveDown(canvas.height);
+        if (event.key === "w") keys.w = true;
+        if (event.key === "s") keys.s = true;
+        if (event.key === "ArrowUp") keys.Up = true;
+        if (event.key === "ArrowDown") keys.Down = true;
     }
 });
+
+window.addEventListener('keyup', (event) => {
+    if (isRunning) {
+        if (event.key === "w") keys.w = false;
+        if (event.key === "s") keys.s = false;
+        if (event.key === "ArrowUp") keys.Up = false;
+        if (event.key === "ArrowDown") keys.Down = false;
+    }
+});
+
+function gameLoop() {
+    if (!isRunning) return;
+    updateGame();
+    renderGame();
+    requestAnimationFrame(gameLoop);
+}
 
 gameLoop();
